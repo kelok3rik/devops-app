c6a367a449b3aefa63142909bbb479b26039892a
Erik <erikrdbs@Gmail.com>
2026-02-10 14:51:03 -0400

    Initial commit - DevOps app

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end(JSON.stringify({ message: "Hola Erik DevOps 🔥" }));
+});
+
+
+server.listen(3000, () => {
+  console.log("Servidor corriendo en puerto 3000");
+});
+
+
+
+
